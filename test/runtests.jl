005fa7b4e956db81e9e05e5c06b41ce9d3191d5a
mwsohn <mwsohn@gmail.com>
2025-11-24 21:03:53 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,13 @@ rename!(mtcars, Pair.(names(mtcars), lowercase.(names(mtcars))))
 mtcars.vsca = recode(mtcars.vs, (0 => "Straight", 1 => "V")...)
 mtcars.vsca = categorical(mtcars.vsca, ordered=true)
 
-# @testset "Convert a DataFrame dataset to Stata .dta file" begin
-    write_stata("mtcars.dta", mtcars)
+# convert mtcars to a stata dta file
+write_stata("mtcars.dta", mtcars)
 
-    df1 = read_stata("mtcars.dta",keep_original=true)
+# convert it back to a DataFrame
+df1 = read_stata("mtcars.dta")
+
+@testset "Convert a DataFrame dataset to Stata .dta file" begin
 
     # number of rows and columns should match
     @test size(mtcars) == size(df1)
@@ -20,7 +23,8 @@ mtcars.vsca = categorical(mtcars.vsca, ordered=true)
     # frequency of mtcars.model and df1.model should match (they are in different types!)
     @test freqtable(mtcars.model) == freqtable(df1.model)
 
-    # all columns must also match
+    # all columns must also match. Because data types may not match,
+    # we will compare means and standard deviations
     @test mean_and_std(mtcars[!, :mpg]) == mean_and_std(df1[!, :mpg])
     @test mean_and_std(mtcars[!, :cyl]) == mean_and_std(df1[!, :cyl])
     @test mean_and_std(mtcars[!, :disp]) == mean_and_std(df1[!, :disp])
@@ -28,18 +32,9 @@ mtcars.vsca = categorical(mtcars.vsca, ordered=true)
     @test mean_and_std(mtcars[!, :drat]) == mean_and_std(df1[!, :drat])
     @test mean_and_std(mtcars[!, :wt]) == mean_and_std(df1[!, :wt])
     @test mean_and_std(mtcars[!, :qsec]) == mean_and_std(df1[!, :qsec])
-@test mean_and_std(mtcars[!, :vs]) == mean_and_std(df1[!, :vs])
-@test mean_and_std(mtcars[!, :am]) == mean_and_std(df1[!, :am])
-@test mean_and_std(mtcars[!, :gear]) == mean_and_std(df1[!, :gear])
-@test mean_and_std(mtcars[!, :carb]) == mean_and_std(df1[!, :carb])
-@test mean_and_std(mtcars[!, :drat]) == mean_and_std(df1[!, :drat])
-tab(mtcars,:vsca)
-freqtable(df1,:vsca)
-
-
-
-# end
-using Stella
-descr(mtcars)
-descr(df1)
+    @test mean_and_std(mtcars[!, :vs]) == mean_and_std(df1[!, :vs])
+    @test mean_and_std(mtcars[!, :am]) == mean_and_std(df1[!, :am])
+    @test mean_and_std(mtcars[!, :gear]) == mean_and_std(df1[!, :gear])
+    @test mean_and_std(mtcars[!, :carb]) == mean_and_std(df1[!, :carb])
+end
 
